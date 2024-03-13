7b0036d398079a467f8850048d9d65428de1d4c3
Denis Voitenko <denis@denvot.dev>
2024-03-13 21:48:10 +0300

    Initial commit

---
+++ src/main/java/com/mipt/hsse/hssetechbackend/HsseTechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mipt.hsse.hssetechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsseTechBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HsseTechBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mipt/hsse/hssetechbackend/HsseTechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mipt.hsse.hssetechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsseTechBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
