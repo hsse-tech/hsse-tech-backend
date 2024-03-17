4c971644bf312637d7a6bdbfbdee1e7d2440d782
Fisman Maxim <mfisman05@gmail.com>
2024-03-17 20:33:09 +0300

    Declared processors and functional interfaces and added some docs for them

--- src/main/java/com/mipt/hsse/hssetechbackend/rent/RentProcessing/DeleteRentProcessing/DeleteRentProcessor.java
+++ src/main/java/com/mipt/hsse/hssetechbackend/rent/RentProcessing/DeleteRentProcessing/DeleteRentProcessor.java
@@ -1,5 +1,11 @@
 package com.mipt.hsse.hssetechbackend.rent.RentProcessing.DeleteRentProcessing;
 
+/**
+ * Provides interface to perform any required actions before a rent gets deleted from the rent database.<br>
+ * The processors are launched as a single transaction.
+ * Therefore, any exception thrown in processCreate() will result in the rollback of the whole transaction and will mean the failure of the removal of the rent.
+ */
+@FunctionalInterface
 public interface DeleteRentProcessor {
-  void processDelete(DeleteRentProcessData request);
+  void processDelete(DeleteRentProcessData deleteRentData);
 }
