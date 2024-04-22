cc05c052a941fd4223caebaece85efcc853a11ac
Denis Voitenko <denis@denvot.dev>
2024-04-22 18:05:41 +0300

    Change Tinkoff API Client interface with signable request type

--- src/main/java/com/mipt/hsse/hssetechbackend/payments/providers/tinkoff/helpers/TinkoffApiClient.java
+++ src/main/java/com/mipt/hsse/hssetechbackend/payments/providers/tinkoff/helpers/TinkoffApiClient.java
@@ -1,14 +1,15 @@
 package com.mipt.hsse.hssetechbackend.payments.providers.tinkoff.helpers;
 
+import com.mipt.hsse.hssetechbackend.payments.providers.tinkoff.entities.requests.TinkoffRequestBase;
 import com.mipt.hsse.hssetechbackend.payments.providers.tinkoff.entities.responses.TinkoffResponse;
 
 /**
  * REST-клиент для Tinkoff API, который поддерживает <a href="https://www.tinkoff.ru/kassa/dev/payments/#section/Podpis-zaprosa">подпись запроса</a>.
  */
 public interface TinkoffApiClient {
-  <T> TinkoffResponse<T> get(String route, Object payload, SerializationMode serializationMode);
-  <T> TinkoffResponse<T> post(String route, Object payload, SerializationMode serializationMode);
-  <T> TinkoffResponse<T> delete(String route, Object payload, SerializationMode serializationMode);
-  <T> TinkoffResponse<T> put(String route, Object payload, SerializationMode serializationMode);
-  <T> TinkoffResponse<T> patch(String route, Object payload, SerializationMode serializationMode);
+  <T> TinkoffResponse<T> get(String route, TinkoffRequestBase payload, SerializationMode serializationMode);
+  <T> TinkoffResponse<T> post(String route, TinkoffRequestBase payload, SerializationMode serializationMode);
+  <T> TinkoffResponse<T> delete(String route, TinkoffRequestBase payload, SerializationMode serializationMode);
+  <T> TinkoffResponse<T> put(String route, TinkoffRequestBase payload, SerializationMode serializationMode);
+  <T> TinkoffResponse<T> patch(String route, TinkoffRequestBase payload, SerializationMode serializationMode);
 }
