ac91ac3b71865fb9ad0352699134952ed05c2612
Denis Voitenko <denis@denvot.dev>
2024-04-29 17:47:11 +0300

    Refactor

--- src/test/java/com/mipt/hsse/hssetechbackend/payments/providers/tinkoff/signing/RequestsSignerTerminalSignTest.java
+++ src/test/java/com/mipt/hsse/hssetechbackend/payments/providers/tinkoff/signing/RequestsSignerTerminalSignTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RequestsSignerTest {
+class RequestsSignerTerminalSignTest {
   private static RequestsSigner signer;
 
   @BeforeAll
