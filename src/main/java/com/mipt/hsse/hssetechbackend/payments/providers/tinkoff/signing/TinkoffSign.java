2cc009aed27303b9bc23ef8fd3325aab5185c3da
Denis Voitenko <denis@denvot.dev>
2024-05-06 18:17:50 +0300

    Fix using TinkoffSign

--- src/main/java/com/mipt/hsse/hssetechbackend/payments/providers/tinkoff/signing/TinkoffSign.java
+++ src/main/java/com/mipt/hsse/hssetechbackend/payments/providers/tinkoff/signing/TinkoffSign.java
@@ -1,10 +1,13 @@
 package com.mipt.hsse.hssetechbackend.payments.providers.tinkoff.signing;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Помечает надобность подписи запроса
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface TinkoffSign {}
