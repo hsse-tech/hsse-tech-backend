f00476594c683cdd496c36f8e822f5029a7691c8
Denis Voitenko <denis@denvot.dev>
2024-05-07 14:29:03 +0300

    Controller

---
+++ src/main/java/com/mipt/hsse/hssetechbackend/payments/controllers/PaymentsController.java
@@ -0,0 +1,9 @@
+package com.mipt.hsse.hssetechbackend.payments.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/payment/top-up-balance")
+public class PaymentsController {
+}
