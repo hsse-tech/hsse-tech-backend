9af86cc629c9f6bc544da95e20fb04f2b734dfe1
Denis Voitenko <denis@denvot.dev>
2024-05-07 14:32:37 +0300

    Requests entity

--- src/main/java/com/mipt/hsse/hssetechbackend/payments/controllers/PaymentsController.java
+++ src/main/java/com/mipt/hsse/hssetechbackend/payments/controllers/PaymentsController.java
@@ -1,9 +1,16 @@
 package com.mipt.hsse.hssetechbackend.payments.controllers;
 
+import com.mipt.hsse.hssetechbackend.payments.controllers.requests.TopUpBalanceRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/payment/top-up-balance")
+@RequestMapping("/api/payment")
 public class PaymentsController {
+  @PostMapping("top-up-balance")
+  public void topUpBalance(@RequestBody TopUpBalanceRequest topUpBalanceRequest) {
+
+  }
 }
