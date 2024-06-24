dec87c46db77bf3f0bde06118afe73e97104a6f8
Denis Voitenko <denis@denvot.dev>
2024-06-24 22:18:20 +0300

    Autoregistration

--- src/main/java/com/mipt/hsse/hssetechbackend/oauth/services/MiptOAuth2UserService.java
+++ src/main/java/com/mipt/hsse/hssetechbackend/oauth/services/MiptOAuth2UserService.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 
 @Service
 public class MiptOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-    private static final String LOGIN_ATTR = "login";
+    private static final String EMAIL_ATTR = "default_email";
     private static final String NAME_ATTR = "id";
 
     private final DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
@@ -27,17 +27,15 @@ public class MiptOAuth2UserService implements OAuth2UserService<OAuth2UserReques
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         var loaded = defaultOAuth2UserService.loadUser(userRequest);
-        var yandexIdObj = loaded.getName();
 
         try {
-            var yandexId = Long.parseLong(yandexIdObj);
-            var yandexLogin = Objects.requireNonNull(loaded.getAttribute(LOGIN_ATTR)).toString();
+            var yandexEmail = Objects.requireNonNull(loaded.getAttribute(EMAIL_ATTR)).toString();
 
-            if (!PhystechDomainValidator.isValid(yandexLogin)) {
+            if (!PhystechDomainValidator.isValid(yandexEmail)) {
                 throw new OAuth2AuthenticationException("Invalid user profile (not \"@phystech.edu\" email suffix)");
             }
 
-            var passport = passportService.findOrCreateByYandexId(yandexId);
+            var passport = passportService.findOrCreateByYandexId(loaded);
             var authorities = passport.getRoles()
                     .stream()
                     .map(role -> new SimpleGrantedAuthority(role.getName()))
---
+++ src/test/java/com/mipt/hsse/hssetechbackend/oauth/services/UserPassportServiceTest.java
@@ -0,0 +1,75 @@
+package com.mipt.hsse.hssetechbackend.oauth.services;
+
+import com.mipt.hsse.hssetechbackend.DatabaseSuite;
+import com.mipt.hsse.hssetechbackend.data.entities.HumanUserPassport;
+import com.mipt.hsse.hssetechbackend.data.repositories.JpaHumanUserPassportRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@Import(UserPassportService.class)
+class UserPassportServiceTest extends DatabaseSuite {
+    @Autowired
+    private JpaHumanUserPassportRepository passportRepository;
+
+    @Autowired
+    private UserPassportService passportService;
+
+    @AfterEach
+    public void clear() {
+        passportRepository.deleteAll();
+    }
+
+    @Test
+    public void testFindAlreadyExists() {
+        passportRepository.save(new HumanUserPassport(123L, "Денис", "Войтенко", "voitenko.da@phystech.edu"));
+
+        var targetPassport = passportService.findOrCreateByYandexId(
+                createUser(123L, "Денис", "Войтенко", "voitenko.da@phystech.edu"));
+
+        assertNotNull(targetPassport);
+        assertTrue(passportRepository.existsById(targetPassport.getId()));
+    }
+
+    @Test
+    public void testFindNotFoundShouldBeCreated() {
+        var targetPassport = passportService.findOrCreateByYandexId(
+                createUser(123L, "Денис", "Войтенко", "voitenko.da@phystech.edu"));
+
+        assertNotNull(targetPassport);
+        assertTrue(passportRepository.existsById(targetPassport.getId()));
+
+        targetPassport = passportRepository.findByYandexId(123L);
+
+        assertEquals("Денис", targetPassport.getFirstName());
+        assertEquals("Войтенко", targetPassport.getLastName());
+        assertEquals("voitenko.da@phystech.edu", targetPassport.getEmail());
+    }
+
+    private OAuth2User createUser(Long yandexId, String firstName, String lastName, String email) {
+        var attrs = new HashMap<String, Object>();
+
+        attrs.put("id", yandexId.toString());
+        attrs.put("default_email", email);
+        attrs.put("first_name", firstName);
+        attrs.put("last_name", lastName);
+
+        return new DefaultOAuth2User(new ArrayList<>(), attrs, "id");
+    }
+}
